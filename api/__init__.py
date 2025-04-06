97c8292a50b2e186ee9737ca34b5ac1988abb036
kajeme11 <chenequek@gmail.com>
2025-04-06 00:10:41 -0700

    get access token function

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,45 @@
+import json
+
 from dotenv import load_dotenv
 import os
+import base64
+from requests import post
+import json
 
 load_dotenv()
 
 client_id = os.getenv("CLIENT_ID")
-client_secrete = os.getenv("CLIENT_SECRET")
+client_secret = os.getenv("CLIENT_SECRET")
+# print(client_id, client_secrete)
+
+# Use Spotify's for developers client id and client secret to get access token
+def get_token():
+    authorization = client_id + ":" + client_secret
+    auth_bytes = authorization.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content_Type": "application/x-www-form-urlencoded"
+    }
+
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+# Use access token in Authorization
+def get_auth_header(token):
+    return {"Authorization": "Bearer " + token}
+
+#Search for an artist
+# def find_artist(token, artist):
+#     url = "https://api.spotify.com/v1/search"
+#     headers = get_auth_header(token)
+#     q
 
 
-print(client_id, client_secrete)
+access_token = get_token()
+print(access_token)
